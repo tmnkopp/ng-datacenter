b62181e706e535578c594550ec61e3a761ef8b13
Tim Kopp <tim.kopp@cyber-balance.com>
2020-04-07 20:55:36 -0400

    rfactor

---
+++ src/app/metric/metric.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  templateUrl: './metric.component.html',
+  styleUrls: ['./metric.component.css']
+})
+export class MetricComponent implements OnInit {
+ 
+  private _value: number = 0;                                              
+  private _max: number = 100;
+
+  @Input('used')
+  set value(value:number){
+    if (isNaN(value)) value = 0;                                     
+    this._value = value; 
+  }
+  get value(): number { return this._value; }
+
+  @Input('available')
+  set max(max:number){
+    if (isNaN(max)) max = 0;                                     
+    this._max = max; 
+  }
+  get max(): number { return this._max; }
+ 
+  isDanger() {                                                 
+    return this.value / this.max > 0.7;                      
+  } 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/nodes/nodes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-nodes]',/* attribute app-nodes instead of an element called app-nodes / any valid CSS selector*/
+  templateUrl: './nodes.component.html',
+  styleUrls: ['./nodes.component.css']
+})
+export class NodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
